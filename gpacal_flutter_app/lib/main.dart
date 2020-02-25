bfae6895670de5ed78ca12996746752d8a72b287
chamikaCN <chamikanandasiri97@gmail.com>
2020-02-25 17:49:39 +0530

    module adder to sgpalcal

--- gpacal_flutter_app/lib/main.dart
+++ gpacal_flutter_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './module_adder.dart';
+import './sgpa_calculator.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('GPA cal'),
         ),
-        body: ModuleAdder(),
+        body: SGPACalculator(),
       ),
     );
   }
